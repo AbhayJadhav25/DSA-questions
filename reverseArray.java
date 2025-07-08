5eef7ce6aad70d5434bb04e4fe2ddabcadebd38c
Abhi-205 <abhayjadhav205@gmail.com>
2025-07-08 11:09:11 +0530

    reverse array

---
+++ reverseArray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class reverseArray{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for(int i = 0 ; i<size ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        /*method 1
+        for(int i=size-1 ; i>=0 ; i--){
+            System.out.print(arr[i]+" ");
+        }
+         
+         method 2
+
+        for(int i =0 ; i<size/2 ; i++){
+            int temp = arr[i];
+            arr[i] = arr[size-1-i];
+            arr[size-1-i] = temp;
+        }
+
+        for(int i =0 ; i<size ; i++){
+            System.out.print(arr[i]+" ");
+        }*/
+
+       int reverse[] = new int[size];
+       for(int i = 0 ; i<size ; i++){
+        reverse[i] = arr[size-1-i];
+       }
+
+       for(int i=0 ; i<size ; i++){
+        System.out.print(reverse[i]+" ");
+       }
+    }
+}
