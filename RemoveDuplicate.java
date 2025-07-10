d9b39fffc337fb45677ecd3e9786f2c52bec6a5d
Abhi-205 <abhayjadhav205@gmail.com>
2025-07-10 23:37:16 +0530

    remove duplicate values from an array

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class RemoveDuplicate {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter Size of an array : ");
-        int size = sc.nextInt();
+        int size = scan.nextInt();
         int arr[] = new int[size];
         int newArr[] = new int[size];
         
         //Sort an array first
         System.out.println("Enter Elements in an array : ");
         for(int i=0;i<size ; i++){
-            arr[i] = sc.nextInt();
+            arr[i] = scan.nextInt();
         }
         for(int i=0;i<size-1;i++){
             for(int j=0 ; j<size-1-i ; j++){
@@ -42,6 +42,6 @@ class RemoveDuplicate {
          for(int i=0 ; i<j ;i++){
             System.out.print(newArr[i]+" ");
          }
-         sc.close();
+         scan.close();
     }
 }
