58b10983cf38b90d8515357e00848d074a08be05
Abhi-205 <abhayjadhav205@gmail.com>
2025-10-26 00:05:44 +0530

    LeetCode 231

---
+++ AddDiigits.java
@@ -0,0 +1,19 @@
+class AddDiigits {
+  public static int addDigits(int num) {
+    while (num >= 10) {
+      int sum = 0;
+      while (num > 0) {
+        sum += (num % 10);
+        num /= 10;
+      }
+      num = sum;
+    }
+    return num;
+  }
+
+  public static void main(String[] args) {
+    int num = 38;
+    int ans = addDigits(num);
+    System.out.println(ans);
+  }
+}
