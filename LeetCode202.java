5336e57a9cef065b51ab49bdfda56f1ad60c0759
Abhi-205 <abhayjadhav205@gmail.com>
2025-10-26 19:08:46 +0530

    Leetcode 202

---
+++ LeetCode202.java
@@ -0,0 +1,24 @@
+//Happy Number
+public class LeetCode202 {
+  public static boolean isHappy(int num) {
+    int sum = num;
+    while (sum > 9) {
+      sum = 0;
+      while (num > 0) {
+        sum += ((num % 10) * (num % 10));
+        num /= 10;
+      }
+      num = sum;
+    }
+    if (sum == 1 || sum == 7)
+      return true;
+    else
+      return false;
+  }
+
+  public static void main(String[] args) {
+    int num = 6;
+    boolean res = isHappy(num);
+    System.out.println(res);
+  }
+}
