6742b25a17adf21e6915da52fe69e412779202f5
Abhi-205 <abhayjadhav205@gmail.com>
2025-10-29 00:00:02 +0530

    Update Moves Zeros to end

--- LeetCode283.java
+++ LeetCode283.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/*import java.util.*;
 
 public class LeetCode283 {
   public static void solution(int nums[]) {
@@ -18,5 +18,32 @@ public class LeetCode283 {
   public static void main(String[] args) {
     int arr[] = { 0, 1, 0, 3, 12 };
     solution(arr);
+  }
+}*/
+
+//Another optimal Solution
+
+import java.util.*;
+
+class LeetCode283 {
+  public static void movesZero(int nums[]) {
+    int n = nums.length;
+    int pos = 0;
+    for (int i = 0; i < n; i++) {
+      if (nums[i] != 0) {
+        if (i != pos) {
+          int temp = nums[i];
+          nums[i] = nums[pos];
+          nums[pos] = temp;
+        }
+        pos++;
+      }
+    }
+    System.out.println(Arrays.toString(nums));
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 0, 1, 0, 3, 12 };
+    movesZero(arr);
   }
 }
