5deb577a22b44a1c609fd89118d8b4c2d24974fe
Abhi-205 <abhayjadhav205@gmail.com>
2025-10-28 23:15:34 +0530

    Running sum of 1D Array

---
+++ LeetCode1480.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class LeetCode1480 {
+  public static int[] solution(int arr[]) {
+    int n = arr.length;
+    for (int i = 1; i < n; i++) {
+      arr[i] += arr[i - 1];
+    }
+    return arr;
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 1, 2, 3, 4 };
+    System.out.println(Arrays.toString(solution(arr)));
+  }
+}
