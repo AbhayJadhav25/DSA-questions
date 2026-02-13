955e5a69673ef221b744e8e92fcce9abfce764d1
Abhi-205 <abhayjadhav205@gmail.com>
2026-02-13 19:27:15 +0530

    No commit

--- Array.java
+++ Array.java
@@ -44,6 +44,8 @@ public class Array {
       int temp = arr[left];
       arr[left] = arr[right];
       arr[right] = temp;
+      left++;
+      right--;
     }
 
     for (int i = 0; i < arr.length; i++) {
@@ -55,6 +57,8 @@ public class Array {
     int arr[] = { 1, 2, 3, 2, 1, 5, 6 };
     FindDuplicate(arr);
     findLargest(arr);
-    reverseArray(arr)
+
+    int arr2[] = { 21, 56, 43, 12, 34, 98, 87 };
+    reverseArray(arr2);
   }
 }
