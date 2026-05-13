69625766a3eaa232bf4aab3e2b55ed2238fd99e0
Abhi-205 <abhayjadhav205@gmail.com>
2026-05-13 23:58:17 +0530

    Array Reverse

---
+++ Java/Array/ArrayReverse_4.java
@@ -0,0 +1,22 @@
+class ArrayReverse_4{
+    public static int[] reverseArray(int arr[]){
+        int left = 0 , right = arr.length -1 ;
+
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return arr;
+    }
+    public static void main(String a[]){
+        int arr[] = {2,4,6,8,9};
+        int ans[] = reverseArray(arr);
+
+        for(int n :ans){
+            System.out.print(n+" ");
+        }
+    }
+}
