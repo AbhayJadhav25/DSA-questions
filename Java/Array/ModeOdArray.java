812b8e1ef93e9695ed3f88a6a97eb933cb057e94
Abhi-205 <abhayjadhav205@gmail.com>
2026-05-14 12:11:19 +0530

    Mode of Array using Hashmap

---
+++ Java/Array/ModeOdArray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class ModeOdArray {
+    static int modeOfArray(int arr[]){
+        int mode;
+        HashMap<Integer , Integer> freq =new  HashMap<>();
+        
+        for(int num : arr){
+            freq.put(num , freq.getOrDefault(num , 0) +1);
+        }
+        System.out.println(freq);
+
+        int maxFreq = -1;
+        int maxKeyFreq = -1;
+
+        for(int key : freq.keySet()){
+            int currentKey = key;
+            int currentKeyFreq = freq.get(currentKey);
+
+            if(currentKeyFreq > maxKeyFreq){
+                maxKeyFreq = currentKeyFreq;
+            }
+        }
+        return maxKeyFreq;
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,2,3,2,3,1,2,9};
+    
+        int ans =  modeOfArray(arr);
+        System.out.println("Mode of Array = "+ans);
+    }
+}
