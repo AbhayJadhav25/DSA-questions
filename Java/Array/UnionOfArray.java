b70b9e8e8e5ecee7a535371b5f19e1e32a900bef
Abhi-205 <abhayjadhav205@gmail.com>
2026-05-14 14:05:56 +0530

    Union of Array

---
+++ Java/Array/UnionOfArray.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+public class UnionOfArray {
+
+    static void unionOfArray(int arr1[] , int arr2[]){
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int num : arr1){
+            set.add(num);
+        }
+
+        for(int num : arr2){
+            if(!(set.contains(num))){
+                set.add(num);
+            }
+        }
+        System.out.println(set);
+    }
+    public static void main(String[] args) {
+        int arr1[] = {1,2,4,2,1,5};
+        int arr2[] = {1,3,6,5,2,4,7};
+        unionOfArray(arr1,arr2);
+    }
+}
