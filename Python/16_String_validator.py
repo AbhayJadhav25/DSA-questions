63da39f14fca24632e350baff6f1ab0e8506ee03
Abhi-205 <abhayjadhav205@gmail.com>
2026-06-20 00:04:31 +0530

    Hacker Rank Problem

---
+++ Python/16_String_validator.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    s = 'qA2'
+    
+    print1 = s.isalnum()
+    print2 = s.isalpha()
+    print3 = s.isdigit()
+    print4 = s.islower()
+    print5 = s.isupper()
+    
+    if any(s.islower) :
+        print(True)
